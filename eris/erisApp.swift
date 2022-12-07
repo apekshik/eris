d1f5a249973f8b2ea1c7bfd5b4b5407f76ffe6bc
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2022-12-07 15:34:45 -0600

    Initial Commit

---
+++ eris/erisApp.swift
@@ -0,0 +1,17 @@
+//
+//  erisApp.swift
+//  eris
+//
+//  Created by Apekshik Panigrahi on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct erisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
