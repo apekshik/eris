807615576d250efc334b188d7d872cd998ef0269
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2022-12-11 13:07:24 -0600

    connected firebase auth and added login/signup functionality to LoginView

--- eris/LoginView.swift
+++ eris/LoginView.swift
@@ -6,11 +6,17 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct LoginView: View {
   @State var isLoginMode = false
   @State private var email: String = ""
   @State private var password: String = ""
+  
+  init() {
+    FirebaseApp.configure()
+  }
+  
   var body: some View {
     VStack {
       Text("ERIS")
@@ -39,7 +45,7 @@ struct LoginView: View {
       }
       
       Button {
-        
+        handleAction()
       } label: {
         HStack {
           Spacer()
@@ -55,8 +61,47 @@ struct LoginView: View {
     }
   }
   
-  func login() {
-    
+  private func handleAction() {
+    if isLoginMode {
+      print("Logging into firebase with existing creds.")
+      loginUser()
+    } else {
+      print("registering new account inside of firebase")
+      createAccount()
+    }
+  }
+  
+  // State var to catch any login/signup errors that we might encounter.
+  @State var loginStatusMessage: String = ""
+  
+  private func loginUser() {
+    Auth.auth().signIn(withEmail: email, password: password) { result, err in
+      if let err = err {
+        // handle error
+        print("failed to login user", err.localizedDescription)
+        return
+      }
+      
+      print("succesffuly logged in user! \(result?.user.uid ?? "")")
+      self.loginStatusMessage = "succesffuly logged in user! User ID: \(result?.user.uid ?? "")"
+      
+    }
+  }
+
+  
+  private func createAccount() {
+    Auth.auth().createUser(withEmail: email, password: password) { result, err in
+      if let err = err {
+        // handle error
+        print("failed to create user", err.localizedDescription)
+        return
+      }
+      
+      print("succesffuly created user! \(result?.user.uid ?? "")")
+      
+      self.loginStatusMessage = "succesffuly created user! User ID:  \(result?.user.uid ?? "")"
+
+    }
   }
 }
 
