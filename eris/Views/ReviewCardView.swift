3a4562e57fbb6545969f2d8bab8a24736f0a6da8
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2022-12-31 12:09:23 -0600

    Just added frontend like button functionality

--- eris/Views/ReviewCardView.swift
+++ eris/Views/ReviewCardView.swift
@@ -11,10 +11,15 @@ struct ReviewCardView: View {
     @State var user: User
     @State var review: Review
     @State var showName: Bool
+    @State var liked: Bool = false
+    @State var scale: CGSize = CGSize(width: 1, height: 1)
+    @State var fullReview: Bool = false
     var body: some View {
         VStack {
             HStack {
                 VStack(alignment: .leading, spacing: 10) {
+                    
+                    
                     HStack {
                         Text(showName ? user.fullName : "")
                             .font(.headline)
@@ -24,19 +29,35 @@ struct ReviewCardView: View {
                             .foregroundColor(.secondary)
                             .frame(maxWidth: .infinity, alignment: .trailing)
                     }
+                    
+                    // written review
                     Text(review.comment)
                         .font(.title)
                         .fontWeight(.black)
                         .foregroundColor(.primary)
                         .lineLimit(3)
+                    
+                    // HStack under the written review.
                     HStack {
                         Text("Written by a \(review.relation)".uppercased())
                             .font(.caption)
                             .foregroundColor(.secondary)
-                        HStack {
-                            Image(systemName: "bubble.right")
-                            Image(systemName: "heart")
-                                .scaleEffect(1.1)
+                        HStack(spacing: 20) {
+                            // Button for comments section.
+                            Button {
+                                // Action trigger when comments section button is pressed.
+                            } label: {
+                                Image(systemName: "bubble.right")
+                            }
+                            // Button for Like/Unlike
+                            Button {
+                                liked.toggle()
+                                let impactLight = UIImpactFeedbackGenerator(style: .light)
+                                impactLight.impactOccurred()
+                            } label: {
+                                Image(systemName: liked ? "heart.fill" : "heart")
+                                    .scaleEffect(1.1)
+                            }
                         }
                             .font(.headline)
                             .foregroundColor(.secondary)
@@ -45,11 +66,31 @@ struct ReviewCardView: View {
                 }
             }
             .padding()
-            .background(Color(hex: "#e6e5e1"))
         }
+        .background(Color(hex: "#e6e5e1"))
         .cornerRadius(10)
         .shadow(radius: 5)
         .padding([.top, .horizontal])
+        .scaleEffect(scale)
+        .onTapGesture(count: 2) {
+            liked.toggle()
+            let impactMed = UIImpactFeedbackGenerator(style: .medium)
+            impactMed.impactOccurred()
+        }
+        
+    }
+    
+    // bounce the card on long press.
+    // Haven't used this anywhwere and isn't complete. Come back to it to polish it out.
+    private func bounceAnimation() {
+        let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
+        impactHeavy.impactOccurred()
+        withAnimation(.spring()) {
+            scale = CGSize(width: 1.05, height: 1.05)
+        }
+//        withAnimation(.spring()) {
+//            scale = CGSize(width: 1, height: 1)
+//        }
     }
 }
 
