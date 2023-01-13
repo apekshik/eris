631fedec9ba97af9aafb03a72dfeec90bdc23049
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2023-01-13 12:43:12 -0600

    test commit for broken git

--- eris/Models/CommentModel.swift
+++ eris/Models/CommentModel.swift
@@ -8,16 +8,18 @@
 import Foundation
 
 struct Comment: Identifiable, Codable {
-    var id = UUID()
-    let authorID: String
-    let authorUserName: String
-    let reviewID: String?
-    let content: String
-    
-    enum CodingKeys: String, CodingKey {
-        case authorID
-        case authorUserName
-        case reviewID
-        case content
-    }
+  var id = UUID()
+  let authorID: String
+  let authorUserName: String
+  let reviewID: String?
+  let content: String
+  let createdAt: Date?
+  
+  enum CodingKeys: String, CodingKey {
+    case authorID
+    case authorUserName
+    case reviewID
+    case content
+    case createdAt
+  }
 }
