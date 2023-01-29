cf516e9ee4ceefc0b02b4cb69021f026062a379c
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2023-01-28 22:14:07 -0600

    Created CameaView UIViewControllerRepresentable

--- eris/Views/CameraViews/CameraView.swift
+++ eris/Views/CameraViews/CameraView.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Apekshik Panigrahi on 1/28/23.
 //
+//  Learnt this from YT Tutorial - https://www.youtube.com/watch?v=ZmPJBiwgZoQ
 
 import SwiftUI
 import AVFoundation
@@ -17,10 +18,41 @@ struct CameraView: UIViewControllerRepresentable {
   
   
   func makeUIViewController(context: Context) -> UIViewController {
+    
+    cameraService.start(delegate: context.coordinator) { err in
+      if let err = err {
+        didFinishProcessingPhoto(.failure(err))
+        return
+      }
+    }
+    
     let viewController = UIViewController()
+    viewController.view.backgroundColor = .black
+    viewController.view.layer.addSublayer(cameraService.previewLayer)
+    cameraService.previewLayer.frame = viewController.view.bounds
     return viewController
   }
   
-  
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self, didFinishProcessingPhoto: didFinishProcessingPhoto)
+  }
   func updateUIViewController(_ uiViewController: UIViewController, context: Context) { }
+  
+  class Coordinator: NSObject, AVCapturePhotoCaptureDelegate {
+    let parent: CameraView
+    private var didFinishProcessingPhoto: (Result<AVCapturePhoto, Error>) -> ()
+    
+    init(_ parent: CameraView, didFinishProcessingPhoto: @escaping (Result<AVCapturePhoto, Error>) -> ()) {
+      self.parent = parent
+      self.didFinishProcessingPhoto = didFinishProcessingPhoto
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+      if let error = error {
+        didFinishProcessingPhoto(.failure(error))
+        return
+      }
+      didFinishProcessingPhoto(.success(photo))
+    }
+  }
 }
